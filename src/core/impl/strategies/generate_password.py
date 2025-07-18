b3724058876dce5e3f70ee1c87096da9e95c95f4
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-18 00:35:40 -0300

    refactor: remove unused Math procotol

--- src/core/impl/strategies/generate_password.py
+++ src/core/impl/strategies/generate_password.py
@@ -2,17 +2,14 @@ from ...entities.password import Password
 from ...entities.rule import Rule
 from ...entities.password import Password
 from ...protocols.i_strategy import IStrategy
-from ...protocols.math import Math
 
 
 class GeneratePassword(IStrategy[Password, None]):
     def __init__(
         self,
-        math: Math,
         select_random_rule: IStrategy[Password, Rule],
         select_random_letter: IStrategy[Rule, str],
     ):
-        self._math = math
         self._select_random_rule = select_random_rule
         self._select_random_letter = select_random_letter
 
