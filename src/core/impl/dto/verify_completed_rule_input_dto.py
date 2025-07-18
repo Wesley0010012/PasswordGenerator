1955d43c0223c0f245c140bd3110e9d13dcae88d
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-18 00:40:37 -0300

    feat: add VerifyCompletedRuleInput dto

---
+++ src/core/impl/dto/verify_completed_rule_input_dto.py
@@ -0,0 +1,14 @@
+from ...entities.password import Password
+from ...entities.rule import Rule
+
+
+class VerifyCompletedRuleInputDto:
+    def __init__(self, password: Password, rule: Rule):
+        self._password = password
+        self._rule = rule
+
+    def get_password(self) -> Password:
+        return self._password
+
+    def get_rule(self) -> Rule:
+        return self._rule
