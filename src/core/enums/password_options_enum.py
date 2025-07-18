a49435f23b21fe0933c281711201894b266dbdb5
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-18 00:21:34 -0300

    feat: add password_options_enum

---
+++ src/core/enums/password_options_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class PasswordOptionsEnum(Enum):
+    UPPERCASE = "uppercase"
+    LOWERCASE = "lowercase"
+    DIGITS = "digits"
+    SPECIAL_CHARACTERS = "special_characters"
